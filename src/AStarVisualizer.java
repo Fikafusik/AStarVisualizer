845bfa8e59d0ed2710a52aa60b0d0f60c68f285e
Fikafusik <fikafusik@gmail.com>
2019-07-03 18:55:39 +0300

    visualizer gui class

--- src/AStarVisualizer.java
+++ src/AStarVisualizer.java
@@ -1,6 +1,11 @@
 
-public class AStarVisualizer {
-    public static void main(String[] args) {
-        System.out.println("AStarVisualizer");
+
+import javax.swing.*;
+
+public class AStarVisualizer extends JFrame {
+    public AStarVisualizer() {
+        this.setName("AStarVisualizer");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(200, 200, 200, 200);;
     }
 }
