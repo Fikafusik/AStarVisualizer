de0e473b46c6645641533cb723147d6ac6a824e9
Fikafusik <fikafusik@gmail.com>
2019-07-03 19:30:43 +0300

    prev/next buttons added

--- src/AStarVisualizer.java
+++ src/AStarVisualizer.java
@@ -1,11 +1,19 @@
 
-
 import javax.swing.*;
 
 public class AStarVisualizer extends JFrame {
+    private JButton nextStepButton;
+    private JButton prevStepButton;
+
     public AStarVisualizer() {
         this.setName("AStarVisualizer");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setBounds(200, 200, 200, 200);;
+
+        nextStepButton = new JButton();
+        nextStepButton.setText("->");
+
+        prevStepButton = new JButton();
+        prevStepButton.setText("<-");
     }
 }
