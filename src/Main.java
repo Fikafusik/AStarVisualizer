964b7d3fd22c650a55b1884e2c76964d41d5f06b
Fikafusik <fikafusik@gmail.com>
2019-07-03 18:54:46 +0300

    main class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        AStarVisualizer aStarVisualizer = new AStarVisualizer();
+        aStarVisualizer.setVisible(true);
+    }
+}
