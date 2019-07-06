b132efa7abf00485eb89bb57db97202885fae414
Lyapital <295341@bk.ru>
2019-07-06 19:52:37 +0300

    Lala

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,5 @@
-import com.mxgraph.model.mxGraphModel;
 import com.mxgraph.swing.mxGraphComponent;
-import com.mxgraph.util.*;
+import com.mxgraph.util.mxEvent;
 import org.jgrapht.ListenableGraph;
 import org.jgrapht.ext.JGraphXAdapter;
 import org.jgrapht.graph.DefaultDirectedGraph;
@@ -10,7 +9,6 @@ import org.jgrapht.graph.DefaultListenableGraph;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Collection;
 
 public class Graph {
     private mxGraphComponent component;
@@ -22,19 +20,18 @@ public class Graph {
     public Graph() {
         g = new DefaultListenableGraph<>(new DefaultDirectedGraph<>(DefaultEdge.class));
         jgxAdapter = new JGraphXAdapter<>(g);
-        this.component = new mxGraphComponent(jgxAdapter);
-        mxGraphModel graphModel = (mxGraphModel)component.getGraph().getModel();
-        Collection<Object> cells = graphModel.getCells().values();
-        mxUtils.setCellStyles(component.getGraph().getModel(), cells.toArray(), mxConstants.STYLE_ENDFILL, mxConstants.SHAPE_ELLIPSE);
 
-        this.jgxAdapter.setCellStyle("CURVE");
+        this.component = new mxGraphComponent(jgxAdapter);
+        Object parent = jgxAdapter.getDefaultParent();
+
         this.component.setConnectable(true); //Тыкабельность
         this.component.setEventsEnabled(true);
-//        this.component.setGridVisible(true); //Видимость сетки
 //        this.component.setPageVisible(true); //Видимость страницы
 
-        g.addVertex("v" + inc++);
+        Object c = jgxAdapter.insertVertex(parent, null, "v" + inc++, 20, 20, 20, 20, "shape=ellipse");
 
+//        jgxAdapter.getCellToVertexMap();
+//        jgxAdapter.getVertexToCellMap();
         this.component.getViewport().setOpaque(true);
         this.component.getViewport().setBackground(new Color(155, 208, 249));
 
@@ -42,37 +39,38 @@ public class Graph {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
                 if(mouseEvent.getClickCount() == 2) {
-                    g.addVertex("v" + inc++);
-//                mouseEvent.getLocationOnScreen()
-                    System.out.println("HELLO");
+                    jgxAdapter.insertVertex(parent, null, "v" + inc++, mouseEvent.getX(), mouseEvent.getY(), 20, 20 , "shape=ellipse");
+                }
+                if(mouseEvent.getClickCount() == 1) {
+
+                    if(mouseEvent.getButton() == mouseEvent.BUTTON3) {
+                        Object cell = component.getCellAt(mouseEvent.getX(), mouseEvent.getY());
+                        if (cell != null) {
+
+                            System.out.println("cell=" + jgxAdapter.getLabel(cell));
+                        }
+                    }
                 }
                 super.mouseClicked(mouseEvent);
             }
         });
 
-//        this.component.addMouseListener(new MouseEvents());
-        this.component.addMouseWheelListener(mouseWheelEvent -> {
+/*        this.component.getGraphControl().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+            }
+        });
+*/        this.component.addMouseWheelListener(mouseWheelEvent -> {
             if(mouseWheelEvent.getWheelRotation() < 0) {
                 component.zoomIn();
             }
             else
                 component.zoomOut();
         });
-        this.component.getConnectionHandler().addListener(mxEvent.CONNECT, new mxEventSource.mxIEventListener() {
-            public void invoke(Object sender, mxEventObject evt) {
-                System.out.println("edge=" + evt.getProperties().toString());
-            }
-        });
+        this.component.getConnectionHandler().addListener(mxEvent.CONNECT, (sender, evt) -> System.out.println("edge=" + evt.getProperties().toString()));
 
-//        MouseEvent event = new Mous;
-//      this.component.getConnectionHandler().mousePressed(event);
-
-        this.component.addListener(mxEvent.ADD_CELLS, new mxEventSource.mxIEventListener() {
-            @Override
-            public void invoke(Object o, mxEventObject mxEventObject) {
-                System.out.println("cell - " + mxEventObject.getName() + " with properties: " + mxEventObject.getProperties());
-            }
-        });
+//        this.component.addListener(mxEvent.ADD_CELLS, (o, mxEventObject) -> System.out.println("cell - " + mxEventObject.getName() + " with properties: " + mxEventObject.getProperties()));
 
     }
 
