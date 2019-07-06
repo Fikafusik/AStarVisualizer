c101aaa40072b7f06ea0a4bd0e92c08129cf88c2
Minullin Michael <fikafusik@gmail.com>
2019-07-07 00:53:38 +0300

    put a cute background :)

---
+++ src/AStarAlgorithm.java
@@ -0,0 +1,22 @@
+
+import com.mxgraph.view.mxGraph;
+
+public class AStarAlgorithm {
+    private mxGraph graph;
+
+    public AStarAlgorithm() {
+
+    }
+
+    public void stepNext() {
+
+    }
+
+    public void stepPrevious() {
+
+    }
+
+    public void reset() {
+
+    }
+}
