b1eacd63381633c0734ebf5989d36fbc76ff97f2
Lyapital <295341@bk.ru>
2019-07-07 16:14:55 +0300

    "issue11 done"

--- src/AStarVisualizer.java
+++ src/AStarVisualizer.java
@@ -16,6 +16,8 @@ public class AStarVisualizer {
     private JGraphXAdapter<String, DefaultEdge> jgxAdapter;
     private ListenableGraph<String, DefaultEdge> g;
     private static int inc = 1;
+    private static int widthDefault = 40;
+    private static String styleDefault = "shape=ellipse";
 
     public AStarVisualizer() {
         g = new DefaultListenableGraph<>(new DefaultDirectedGraph<>(DefaultEdge.class));
@@ -24,13 +26,12 @@ public class AStarVisualizer {
         this.graphComponent = new mxGraphComponent(jgxAdapter);
         Object parent = jgxAdapter.getDefaultParent();
 
-        this.graphComponent.setConnectable(true); //�������������
+        this.graphComponent.setConnectable(true);
         this.graphComponent.setEventsEnabled(true);
-        // this.graphComponent.setPageVisible(true); //��������� ��������
+        // this.graphComponent.setPageVisible(true);
 
-        // TODO: смотреть нижний TODO
-        Object c = jgxAdapter.insertVertex(parent, null, "v" + inc++, 20, 20, 30, 30, "shape=ellipse");
-        Object j = jgxAdapter.insertVertex(parent, null, "v" + inc++, 40, 40, 30, 30, "shape=ellipse");
+        Object c = jgxAdapter.insertVertex(parent, null, "v" + inc++, 20, 20, widthDefault, widthDefault, styleDefault);
+        Object j = jgxAdapter.insertVertex(parent, null, "v" + inc++, 60, 60, widthDefault, widthDefault, styleDefault);
         jgxAdapter.setAllowDanglingEdges(false);
 
         jgxAdapter.setCellsEditable(false);
@@ -42,8 +43,7 @@ public class AStarVisualizer {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
                 if (mouseEvent.getClickCount() == 2) {
-                    // TODO: нужно вынести магические константы (как вариант: радиус вершины передавать в конструктор
-                    jgxAdapter.insertVertex(parent, null, "v" + inc++, mouseEvent.getX()-15, mouseEvent.getY()-15, 30, 30 , "shape=ellipse");
+                    jgxAdapter.insertVertex(parent, null, "v" + inc++, mouseEvent.getX()-widthDefault/2, mouseEvent.getY()-widthDefault/2, widthDefault, widthDefault , styleDefault);
                 }
                 if (mouseEvent.getClickCount() == 1) {
                     if (mouseEvent.getButton() == mouseEvent.BUTTON3) {
