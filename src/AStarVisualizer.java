7bc1eaa5658b185db9a156e3900452bc4de28053
Lyapital <295341@bk.ru>
2019-07-07 20:10:50 +0300

    Synchronizaition

--- src/AStarVisualizer.java
+++ src/AStarVisualizer.java
@@ -1,4 +1,6 @@
+import com.mxgraph.model.mxCell;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
 import com.mxgraph.util.mxEvent;
 import org.jgrapht.ListenableGraph;
 import org.jgrapht.ext.JGraphXAdapter;
@@ -9,6 +11,7 @@ import org.jgrapht.graph.DefaultListenableGraph;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Hashtable;
 
 public class AStarVisualizer {
     private mxGraphComponent graphComponent;
@@ -18,53 +21,59 @@ public class AStarVisualizer {
     private static int inc = 1;
     private static int widthDefault = 40;
     private static String styleDefault = "shape=ellipse";
+    Object parent;
+    MouseAdapter addDelVertex;
 
     public AStarVisualizer() {
         g = new DefaultListenableGraph<>(new DefaultDirectedGraph<>(DefaultEdge.class));
+//        g.get
         jgxAdapter = new JGraphXAdapter<>(g);
 
         this.graphComponent = new mxGraphComponent(jgxAdapter);
-        Object parent = jgxAdapter.getDefaultParent();
+        parent = jgxAdapter.getDefaultParent();
 
         this.graphComponent.setConnectable(true);
         this.graphComponent.setEventsEnabled(true);
-        // this.graphComponent.setPageVisible(true);
 
-        Object c = jgxAdapter.insertVertex(parent, null, "v" + inc++, 20, 20, widthDefault, widthDefault, styleDefault);
-        Object j = jgxAdapter.insertVertex(parent, null, "v" + inc++, 60, 60, widthDefault, widthDefault, styleDefault);
+        jgxAdapter.insertVertex(parent, null, "v" + inc++, 20, 20, widthDefault, widthDefault, styleDefault);
+        jgxAdapter.insertVertex(parent, null, "v" + inc++, 60, 60, widthDefault, widthDefault, styleDefault);
         jgxAdapter.setAllowDanglingEdges(false);
-
         jgxAdapter.setCellsEditable(false);
 
-        this.graphComponent.getViewport().setOpaque(true);
-        this.graphComponent.getViewport().setBackground(new Color(155, 208, 249));
+        graphComponent.getViewport().setOpaque(true);
 
-        this.graphComponent.getGraphControl().addMouseListener(new MouseAdapter() {
+        this.graphComponent.getViewport().setOpaque(true);
+        addDelVertex = new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
                 if (mouseEvent.getClickCount() == 2) {
-                    jgxAdapter.insertVertex(parent, null, "v" + inc++, mouseEvent.getX()-widthDefault/2, mouseEvent.getY()-widthDefault/2, widthDefault, widthDefault , styleDefault);
-                }
+                    if(mouseEvent.getButton() == mouseEvent.BUTTON1)
+                        jgxAdapter.insertVertex(parent, null, "v" + inc++, mouseEvent.getX() - 15, mouseEvent.getY() - 15, 30, 30 , "shape=ellipse");
+/*                    if(mouseEvent.getButton() == mouseEvent.BUTTON3) {
+                        Hashtable<String, Object> style = new Hashtable<>();
+                        style.put(mxConstants.STYLE_FILLCOLOR, "#FFFFFF");
+                        style.put();
+                    } //В разработке
+*/                }
                 if (mouseEvent.getClickCount() == 1) {
                     if (mouseEvent.getButton() == mouseEvent.BUTTON3) {
                         Object cell = graphComponent.getCellAt(mouseEvent.getX(), mouseEvent.getY());
                         if (cell != null) {
-                            jgxAdapter.getModel().remove(cell);
-                            System.out.println("cell=" + jgxAdapter.getLabel(cell));
+                            for(Object child : graphComponent.getGraph().getEdges(cell))
+                                jgxAdapter.getModel().remove(child);
+//                            jgxAdapter.getModel().remove(cell);
+//                            System.out.println("cell delete =" + jgxAdapter.getLabel(cell));
                         }
                     }
                 }
                 super.mouseClicked(mouseEvent);
             }
-        });
+        };
 
-        System.out.println(graphComponent.getCells(graphComponent.getLayoutAreaSize().getRectangle()).length);
+        AddDelVertexEble();
 
-        /*
-        for(mxICell cell: jgxAdapter.getCellToVertexMap().keySet()) {
-            System.out.println("OUT : x - " + cell.getGeometry().getPoint().x + ", y - " + cell.getGeometry().getPoint().y);
-        }
-        */
+        this.graphComponent.getViewport().setBackground(new Color(155, 208, 249));
+
 
 /*        this.graphComponent.addMouseWheelListener(mouseWheelEvent -> {
             if (mouseWheelEvent.getWheelRotation() < 0) {
@@ -77,11 +86,17 @@ public class AStarVisualizer {
 
         this.graphComponent.getConnectionHandler().addListener(mxEvent.CONNECT, (sender, evt) -> System.out.println("edge=" + evt.getProperties().toString()));
 
-        // this.graphComponent.addListener(mxEvent.ADD_CELLS, (o, mxEventObject) -> System.out.println("cell - " + mxEventObject.getName() + " with properties: " + mxEventObject.getProperties()));
+        //this.graphComponent.addListener(mxEvent.ADD_CELLS, (o, mxEventObject) -> System.out.println("cell - " + mxEventObject.getName() + " with properties: " + mxEventObject.getProperties()));
     }
 
     public mxGraphComponent getGraphComponent() {
         return graphComponent;
     }
 
+    public void AddDelVertexEble(){
+        this.graphComponent.getGraphControl().addMouseListener(addDelVertex);
+    }
+    public void AddDelVertexRemote() {
+        this.graphComponent.getGraphControl().removeMouseListener(addDelVertex);
+    }
 }
