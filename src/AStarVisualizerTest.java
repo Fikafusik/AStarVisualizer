237376d0fdfd4c9f7f0db804e6f643fa689099f7
Lyapital <295341@bk.ru>
2019-07-11 22:45:44 +0300

    New

---
+++ src/AStarVisualizerTest.java
@@ -0,0 +1,86 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class AStarVisualizerTest {
+    AStarVisualizer aStarVisualizer;
+
+    @Before
+    public void setUp() throws Exception {
+        aStarVisualizer = new AStarVisualizer();
+        File file = new File(".\\Tests\\Test1.xml");
+        if(file.isFile()) {
+            aStarVisualizer.openGraph(file.getAbsolutePath());
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void openGraph(String absolutePath) throws IOException {
+        File file = new File(".\\Tests\\Test1.xml");
+        if(file.isFile()) {
+            aStarVisualizer.openGraph(file.getAbsolutePath());
+        }
+
+    }
+
+    @Test
+    public void saveGraph() {
+    }
+
+    @Test
+    public void getGraphComponent() {
+    }
+
+    @Test
+    public void paintComponent() {
+    }
+
+    @Test
+    public void paintPast() {
+    }
+
+    @Test
+    public void paintNow() {
+    }
+
+    @Test
+    public void paintFuture() {
+    }
+
+    @Test
+    public void paintDefaultComponent() {
+    }
+
+    @Test
+    public void setListenerEditVertex() {
+    }
+
+    @Test
+    public void removeListenerEditVertex() {
+    }
+
+    @Test
+    public void setListenerAddStartFinish() {
+    }
+
+    @Test
+    public void removeListenerAddStartFinish() {
+    }
+
+    @Test
+    public void clearGraph() {
+    }
+
+    @Test
+    public void setSink() {
+    }
+}
