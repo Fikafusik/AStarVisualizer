454ed27f665e4337c6bc87fa3a37c07295fd6b29
Minullin Michael <fikafusik@gmail.com>
2019-07-12 05:59:22 +0300

    TextPaneLogger JavaDoc

--- src/TextPaneLogger.java
+++ src/TextPaneLogger.java
@@ -1,17 +1,33 @@
 import javax.swing.*;
 import javax.swing.text.Document;
 
+/**
+ * Класс, расширяющий возможности стандартной текстовой панели:
+ * добавляет возможность логировать действия внутрь компонента
+ */
 public class TextPaneLogger {
+    /**
+     * Композиция лучше наследования)0))
+     * Графический компонент для вывода логов программы
+     */
     private JTextPane textPane;
 
     public TextPaneLogger(JTextPane textPane){
         setTextPane(textPane);
     }
 
+    /**
+     * Сеттер для текстовой панели
+     * @param textPane экземпляр текстовой панели
+     */
     public void setTextPane(JTextPane textPane){
         this.textPane = textPane;
     }
 
+    /**
+     * Вывод сообщения на текстовую панель
+     * @param msg сообщение для вывода
+     */
     public void log(String msg){
         Document doc = textPane.getDocument();
         try {
