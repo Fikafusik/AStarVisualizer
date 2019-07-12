a2e1fb1b7ed84e1614c68222ef337890b8f206bc
Minullin Michael <fikafusik@gmail.com>
2019-07-12 06:01:22 +0300

    UndoableOperation JavaDoc

--- src/UndoableOperation.java
+++ src/UndoableOperation.java
@@ -1,2 +1,6 @@
+/**
+ * Абстрактный класс, совмещающий интерфейсы операции и отмены операции
+ * Выражает сущность: "отменяемая операция"
+ */
 public abstract class UndoableOperation implements IDoable, IUndoable{
 }
